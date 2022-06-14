47bd76e2a349d37c9e0979f4ca45e86dea222e56
John Franchak <franchak@gmail.com>
2022-06-13 22:05:42 -0700

    initial

---
+++ pub_list.R
@@ -0,0 +1,5 @@
+#remotes::install_github("njahn82/semscholar")
+# library(semscholar)
+# library(tidyverse)
+# paper_ids <- (s2_authors("3207623"))$papers[[1]]
+# my_pubs <- s2_papers(paper_ids[,"paper_id"])
